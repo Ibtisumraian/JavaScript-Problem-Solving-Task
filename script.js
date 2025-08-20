2947e50cdd8e94ad1389e1fe7c45bf2c67a33ad4
Ibtisumraian <ibtisumraian@gmail.com>
2025-08-20 10:07:52 +0600

    Problem 2: Count Vowels in a String

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ console.log(reverseString("hello"));
 
 // * Problem 2: Count Vowels in a String
 
+function countVowels(str) {
+  const vowels = "aeiouAEIOU";
+  let count = 0;
+  for (let word of str) {
+    if (vowels.includes(word)) count++;
+  }
+  return count;
+}
+
+console.log('Vowels Count =>',countVowels("programming"));
