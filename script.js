3f2f48226b357a249584cde78d22d39fa12ad793
Ibtisumraian <ibtisumraian@gmail.com>
2025-08-20 11:01:06 +0600

    Problem 9: Find the Factorial of a Number

--- script.js
+++ script.js
@@ -74,5 +74,17 @@ function capitalizeWords(str) {
 
 console.log(capitalizeWords("hello world"));
 
-// * 
+
+
+// * Problem 9: Find the Factorial of a Number
+function factorial(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(5));
+
 // * 
