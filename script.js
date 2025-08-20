1eb57a2485ee6e3f1017088214bb6ba5d28b69c4
Ibtisumraian <ibtisumraian@gmail.com>
2025-08-20 10:51:52 +0600

    Problem 5: Remove Duplicates from an Array

--- script.js
+++ script.js
@@ -41,7 +41,16 @@ function findMax(arr) {
 console.log(findMax([5, 1, 9, 3])); 
 
 
-// * 
+// * Problem 5: Remove Duplicates from an Array
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(removeDuplicates([1, 1, 2, 2, 3, 3, 4, 4])); 
+
+
+
 // * 
 // * 
 // * 
